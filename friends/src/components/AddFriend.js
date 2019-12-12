1187580fa9b9a65bc2b09ab232bb14d958aa79f7
aleesha-wood <spectrat@spectratart.com>
2019-12-11 20:53:33 -0500

    add friend feature functional, MVP

--- friends/src/components/AddFriend.js
+++ friends/src/components/AddFriend.js
@@ -1,10 +1,40 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Redirect } from 'react-router-dom';
+import api from '../utils/api';
 
 function AddFriend(props) {
+  const [friendData, setFriendData] = useState({
+    name : '',
+    age : '',
+    email : '' 
+  });
+
+  const handleChange = event => {
+    setFriendData({
+      ...friendData, [event.target.name] : event.target.value
+    })
+  };
+
+  const handleSumbit = event => {
+    event.preventDefault();
+
+    api()
+      .post('/api/friends', friendData)
+      .then(result => {
+        console.log(result.data)
+      })
+      .catch(error => {
+        console.log(error)
+      });
+  }
+
+
   return(
-    <form>
-      <input type='text' placeholder='name' />
+    <form onSubmit={handleSumbit}>
+      <input name='name' type='text' placeholder='name' value={friendData.name} onChange={handleChange}/>
+      <input name='age' type='text' placeholder='age' value={friendData.age} onChange={handleChange} />
+      <input name='email' type='email' placeholder='email' value={friendData.email} onChange={handleChange} />
+      <button type='submit'>Add Friend</button>
     </form>
   )
 }
